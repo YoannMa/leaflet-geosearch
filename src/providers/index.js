9508093f57dcc9d3e9b21e35dbcbc794eb35db01
Yoann MALLEMANCHE <yoann.mallemanche@gmail.com>
2018-04-26 11:25:27 +0200

    Update index.js

--- src/providers/index.js
+++ src/providers/index.js
@@ -3,4 +3,5 @@ export { default as EsriProvider } from './esriProvider';
 export { default as GoogleProvider } from './googleProvider';
 export { default as OpenStreetMapProvider } from './openStreetMapProvider';
 export { default as LocationIQProvider } from './locationIQProvider';
+export { default as OpenCageProvider } from './openCageProvider';
 export { default as Provider } from './provider';
